933eced359c69cd8dad6ee07b8fdb4e1532963cf
Fábio Luciano <fabio@naoimporta.com>
2023-05-11 15:28:08 -0300

    add: add more things

--- src/plugin/datetime.sh
+++ src/plugin/datetime.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 plugin_datetime_icon=$(get_tmux_option "@theme_plugin_datetime_icon" "")
-plugin_datetime_accent_color=$(get_tmux_option "@theme_plugin_datetime_accent_color" "green")
-plugin_datetime_accent_color_icon=$(get_tmux_option "@theme_plugin_datetime_accent_color_icon" "green1")
+plugin_datetime_accent_color=$(get_tmux_option "@theme_plugin_datetime_accent_color" "blue7")
+plugin_datetime_accent_color_icon=$(get_tmux_option "@theme_plugin_datetime_accent_color_icon" "blue0")
 
 # https://man7.org/linux/man-pages/man1/date.1.html
 plugin_datetime_format=$(get_tmux_option "@theme_plugin_datetime_format" "%F")
