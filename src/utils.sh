0b686ee22f02ae1ac437b06a1bf8241861b3c07b
Omer Aloni <omer.a@solaredge.com>
2024-08-02 16:05:07 +0300

    feat: added additional icons customization options
    Added:
    - @theme_plugin_active_window_icon
    - @theme_plugin_inactive_window_icon
    - @theme_plugin_zoomed_window_icon
    - @theme_plugin_pane_synchronized_icon

--- src/utils.sh
+++ src/utils.sh
@@ -21,17 +21,26 @@ function generate_left_side_string() {
 }
 
 function generate_inactive_window_string() {
+
+	inactive_window_icon=$(get_tmux_option "@theme_plugin_inactive_window_icon" " ")
+	zoomed_window_icon=$(get_tmux_option "@theme_plugin_zoomed_window_icon" " ")
+
 	local separator_start="#[bg=${PALLETE['dark5']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
 	local separator_internal="#[bg=${PALLETE['dark3']},fg=${PALLETE['dark5']}]${left_separator:?}#[none]"
 	local separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['dark3']}]${left_separator:?}#[none]"
 
-	echo "${separator_start}#[fg=${PALLETE[white]}]#I${separator_internal}#[fg=${PALLETE[white]}] #{?window_zoomed_flag, , }#W ${separator_end}"
+	echo "${separator_start}#[fg=${PALLETE[white]}]#I${separator_internal}#[fg=${PALLETE[white]}] #{?window_zoomed_flag,$zoomed_window_icon,$inactive_window_icon}#W ${separator_end}"
 }
 
 function generate_active_window_string() {
+
+	active_window_icon=$(get_tmux_option "@theme_plugin_active_window_icon" " ")
+	zoomed_window_icon=$(get_tmux_option "@theme_plugin_zoomed_window_icon" " ")
+	pane_synchronized_icon=$(get_tmux_option "@theme_plugin_pane_synchronized_icon" "✵")
+
 	separator_start="#[bg=${PALLETE['magenta']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
 	separator_internal="#[bg=${PALLETE['purple']},fg=${PALLETE['magenta']}]${left_separator:?}#[none]"
 	separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['purple']}]${left_separator:?}#[none]"
 
-	echo "${separator_start}#[fg=${PALLETE[white]}]#I${separator_internal}#[fg=${PALLETE[white]}] #{?window_zoomed_flag, , }#W #{?pane_synchronized,✵, }${separator_end}#[none]"
+	echo "${separator_start}#[fg=${PALLETE[white]}]#I${separator_internal}#[fg=${PALLETE[white]}] #{?window_zoomed_flag,$zoomed_window_icon,$active_window_icon}#W #{?pane_synchronized,$pane_synchronized_icon, }${separator_end}#[none]"
 }
